140033c791f0f15493f21fcb66eb4974c785e6b4
Emin Muhammadi <muemin17631@sabah.edu.az>
2022-06-03 17:30:17 +0200

    Redux added

--- client/src/store/calculator/types.ts
+++ client/src/store/calculator/types.ts
@@ -1,11 +1,7 @@
 import { Action } from 'redux';
 
 export enum CalculatorActionTypes {
-<<<<<<< HEAD
     ADD = "@@calculator/AddAction",
-=======
-    ADD = "ADD",
->>>>>>> 21efb61fe373fb7decd3f29458d4a86906bd7d88
 }
 
 export interface ICalculator {
@@ -14,13 +10,9 @@ export interface ICalculator {
     result: number;
 }
 
-<<<<<<< HEAD
 /**
  * Add Action
  */
-=======
-// Action Types
->>>>>>> 21efb61fe373fb7decd3f29458d4a86906bd7d88
 export interface AddAction extends Action<CalculatorActionTypes.ADD> {
     a: number;
     b: number;
